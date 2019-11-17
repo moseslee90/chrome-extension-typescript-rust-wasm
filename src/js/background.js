db70fb72fab1c2ce521e741b70f4cf0ddd680a27
moseslee90 <moseslee90@gmail.com>
2019-11-17 13:22:35 +0800

    removed js

--- src/js/background.js
+++
@@ -1,7 +0,0 @@
-import "../img/icon-128.png";
-import "../img/icon-34.png";
-
-const js = import("../../pkg-rust-wasm/harlo_wasm.js");
-js.then(js => {
-    js.run();
-});
--- src/js/popup/example.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-    // const js = import(
-    //     "../../../node_modules/@moseslee90/harlo-wasm/harlo_wasm.js"
-    // );
-    // js.then(js => {
-    //     js.run();
-    // });
-}
